4f8df6b838d5f6b170f37c67aa12c34029fc8881
MasterMind <87894877+Master-Mind-007@users.noreply.github.com>
2022-01-10 11:22:43 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+### TELEGRAM MULTIPURPOSE RENAMER BOT By @MasterMind_Mikhil
+
+ABOUT BOT 
+ 🔓 RENAME TELEGRAM FILES WITH PERMANENT THUMBNAIL SUPPORT. 
+ 🔒 CONVERT FILES TO VIDEOS AND VIDEOS TO FILE
+ 🔒 ALSO CAN DOWNLOAD VIDEOS FROM ZEE5. 
+
+
+
+### Deploy Bot By the Below Deploy Button
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Master-Mind-007/File-Renamer)
+ 
+
+### OWNED AND MAINTAINED BY @MasterMind_Mikhil
+
---
+++ translation.py
@@ -0,0 +1,39 @@
+class Translation(object):
+    START_TEXT = """ <b> Hi {} , I am @MasterMind_Mikhil 's Worker. Maintained By the Owner.</b> \n
+
+<b>Bot Maintained and Owned By @MasterMind_Mikhil</b> \n 
+<b> I can also download & upload files from Zee5</b> \n
+<b>Do /help for more Details...</b> \n
+"""
+
+    RENAME_403_ERR = "Sorry but bot responseded by 403 Error!!"
+    BANNED_USER_TEXT = "Hi Bitch why are you still using when you are already Banned!! \n \n Now Fuck Off LOSER!!! " 
+    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    UPGRADE_TEXT = "By @MasterMind_Mikhil "
+    DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
+    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Successfully Completed the work ✅ T/b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "LoL Looser Using Others private Property \n Don't Contact @MasterMind_Mikhil"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "LoL Looser Using Others private Property \n Don't Contact @MasterMind_Mikhil"
+    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail cleared succesfully🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
+    REPLY_TO_FILE_FOR_CONVERT = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_THUMB_FOUND = "No Thumbnail found"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
+    ABOUT_ME = """<b>ABOUT ME : \n @MasterMind_Mikhil 's PERSONAL WORKER. \n FOR FURTHER HELP CONTACT @MasterMind_Mikhil\n \n FOOLISHNES = BAN!! </b>"""
+    HELP_USER = """SIMPLE RULES 
+    
+1.🔹 <b>Send me any Telegram File.</b> \n
+2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
+3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
+4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n
+5.🔹 <b> Send Me Any Zee5 Url And I'll Upload It Into Telegram </b> \n \n <b>OWNED AND MAINTAINED BY @MasterMind_Mikhil </b>"""
+
